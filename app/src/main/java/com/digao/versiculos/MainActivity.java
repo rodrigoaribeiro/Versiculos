173b7c88b94b0d2cf994062fb068c02dc3360025
rodrigoaribeiro <digao.listas@gmail.com>
2024-06-23 13:22:45 -0300

    add listview - not working

--- app/src/main/java/com/digao/versiculos/MainActivity.java
+++ app/src/main/java/com/digao/versiculos/MainActivity.java
@@ -1,6 +1,8 @@
 package com.digao.versiculos;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    // Array of strings...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+                "WebOS","Ubuntu","Windows7","Max OS X"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_main, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.books_list );
+        listView.setAdapter(adapter);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
